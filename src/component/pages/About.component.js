a7602926fd24250dfb9882f0d775fe21cb6da960
geraldy1st <geraldy.leondas@gmail.com>
2020-10-20 12:41:52 +0200

    maj about

--- src/component/pages/About.component.js
+++ src/component/pages/About.component.js
@@ -3,8 +3,15 @@ import React, { Fragment } from 'react';
 function About() {
   return (
     <Fragment>
-      <h1>About</h1>
-      <p>This is the Todos app v1.0.0. It is part of a React crash course</p>
+       <div className="content">
+         <h1>About</h1>
+         <p>This is the Todos app v1.0.0. It is part of a React crash course</p>   
+       
+       </div>
+       <footer className="footer">
+           <p><a href="https://github.com/geraldy1st">Copyright © 2019 Social Network by Geraldy. All rights reserved.</a></p>
+          
+       </footer>
     </Fragment>
   );
 }
